955249710e070575fe8ec4b76a0212b2b4b62e85
CRB112 <connorrb1111@gmail.com>
2025-11-10 12:25:49 -0500

    Changed formatting on home page, changed a few bootstrap color values

--- Proj/app.py
+++ Proj/app.py
@@ -4,16 +4,12 @@ import psycopg2 #install: pip install psycopg2
 
 #DB connection from config
 app = Flask(__name__)
-
-# Connect to the database         //replace with your local db name
 conn = config.conn
 
 # create a cursor
 cur = conn.cursor()
-
 # commit the changes
 conn.commit()
-
 # close the cursor and connection
 cur.close()
 conn.close()
