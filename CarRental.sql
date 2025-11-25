e759022171ad892a2812d23ffbb6f67622ed49cc
CRB112 <connorrb1111@gmail.com>
2025-11-25 17:05:19 -0500

    Removed duplicate SQL queries

--- CarRental.sql
+++ CarRental.sql
@@ -30,45 +30,6 @@ CREATE TABLE "PaymentInfo"(
 );
 ALTER TABLE
     "PaymentInfo" ADD PRIMARY KEY("payment_id");
-CREATE TABLE "Locations"(
-    "location_id" INTEGER NOT NULL,
-    "name" VARCHAR(100) NOT NULL,
-    "street" VARCHAR(150) NOT NULL,
-    "city" VARCHAR(100) NOT NULL,
-    "state" VARCHAR(50) NOT NULL,
-    "postal_code" VARCHAR(20) NOT NULL,
-    "phone_number" VARCHAR(20) NULL
-);
-ALTER TABLE
-    "Locations" ADD PRIMARY KEY("location_id");
-CREATE TABLE "Cars"(
-    "car_id" INTEGER NOT NULL,
-    "location_id" INTEGER NULL,
-    "make" VARCHAR(50) NOT NULL,
-    "model" VARCHAR(50) NOT NULL,
-    "year" INTEGER NOT NULL,
-    "license_plate" VARCHAR(20) NOT NULL,
-    "daily_rate" DECIMAL(10, 2) NOT NULL,
-    "status" VARCHAR(255) CHECK
-        ("status" IN('')) NULL DEFAULT 'available'
-);
-ALTER TABLE
-    "Cars" ADD PRIMARY KEY("car_id");
-ALTER TABLE
-    "Cars" ADD CONSTRAINT "cars_license_plate_unique" UNIQUE("license_plate");
-CREATE TABLE "Reservations"(
-    "reservation_id" INTEGER NOT NULL,
-    "user_id" INTEGER NULL,
-    "car_id" INTEGER NULL,
-    "pickup_location" INTEGER NULL,
-    "dropoff_location" INTEGER NULL,
-    "payment_id" INTEGER NULL,
-    "start_date" DATE NOT NULL,
-    "end_date" DATE NOT NULL,
-    "total_cost" DECIMAL(10, 2) NOT NULL,
-    "status" VARCHAR(255) CHECK
-        ("status" IN('')) NULL DEFAULT 'pending'
-);
 
 CREATE TABLE "Reviews" (
     "review_id" INTEGER NOT NULL,
